9a5c436ac5066ef40f36edf447f37ab3a4d40db5
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-09-29 14:10:07 +0300

    Delete unused files

--- webpack.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var merge = require("webpack-merge").merge;
-var common = require("./webpack.common");
-module.exports = merge(common, {
-    mode: "production",
-});
-//# sourceMappingURL=webpack.prod.js.map
