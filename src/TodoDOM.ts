38bfb1ec3f2534f93a2dcc12d642416555af0452
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-09-30 10:33:43 +0300

    Create class to display Todo-objects

--- src/TodoDOM.ts
+++ src/TodoDOM.ts
@@ -1,20 +1,50 @@
 import ContainerDOM from "./ContainerDOM";
-import { createCheckBox, createDiv, createElement } from "./utility";
+import { createButton, createCheckBox, createDiv } from "./utility";
 
 export default class TodoDOM extends ContainerDOM {
   private _checkBox: HTMLInputElement;
   private _title: HTMLDivElement;
+  private _date: HTMLDivElement;
+  private _detailsButton: HTMLButtonElement;
+  private _editButton: HTMLButtonElement;
+  private _deleteButton: HTMLButtonElement;
 
   constructor(
     checked: boolean,
+    title: string,
+    date: string,
     className: string,
-    id?: string,
-    ...children: HTMLElement[]
+    id?: string
   ) {
-    super(className, id, ...children);
-    this._title = createDiv(`${className}-title`);
+    super("todo");
+
     this._checkBox = createCheckBox(`${className}-checkbox`);
+    this._title = createDiv(`${className}-title`);
+    this._date = createDiv(`${className}-date`);
+    this._detailsButton = createButton(`${className}-button`);
+    this._editButton = createButton(`${className}-button`);
+    this._deleteButton = createButton(`${className}-button`);
+
     this._checkBox.checked = checked;
+    this._title.textContent = title;
+    this._date.textContent = date;
+
+    this.element.append(
+      this._checkBox,
+      this._title,
+      this._date,
+      this._detailsButton,
+      this._editButton,
+      this._deleteButton
+    );
+  }
+
+  public isChecked(): boolean {
+    return this._checkBox.checked;
+  }
+
+  public setChecked(value: boolean) {
+    this._checkBox.checked = value;
   }
 
   public get title(): string | null {
@@ -25,11 +55,11 @@ export default class TodoDOM extends ContainerDOM {
     this._title.textContent = title;
   }
 
-  public isChecked(): boolean {
-    return this._checkBox.checked;
+  public get date(): string | null {
+    return this._date.textContent;
   }
 
-  public setChecked(value: boolean) {
-    this._checkBox.checked = value;
+  public set date(date: string) {
+    this._date.textContent = date;
   }
 }
