c631ea934ac940d6140a9c3364683646a88fb4a1
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-01 12:22:14 +0300

    Allow NameIdentifiable creation with fixed id

--- src/NamedIdentifiable.ts
+++ src/NamedIdentifiable.ts
@@ -4,9 +4,9 @@ export default abstract class NamedIdentifiable {
   private _name: string;
   private readonly _id: string;
 
-  constructor(name: string) {
+  constructor(name: string, id?: string) {
     this._name = name;
-    this._id = RandomIdGenerator();
+    this._id = id ?? RandomIdGenerator();
   }
   public get name(): string {
     return this._name;
