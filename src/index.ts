4cdedad65177da1b4580cf92ad2758123ceae09d
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-04 13:30:49 +0300

    Create and style app elements

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import MainCategory from "./app-logic/MainCategory";
 import Todo from "./app-logic/Todo";
 import TodoDOM from "./DOM/TodoDOM";
 import TodoList from "./app-logic/TodoList";
-import "./sass/nav.scss";
+import "./sass/main.scss";
 import { createNavLogo } from "./app-util/utility";
 createNavLogo();
 
