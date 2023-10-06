6094a5e17bbb95598ef313ebe09559e7b68599d2
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-06 18:40:44 +0300

    remove unused files

--- src/app-logic/MainCategoryList.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-useless-constructor */
-import CategoryList from "./CategoryList";
-import MainCategory from "./MainCategory";
-
-export default class MainCategoryList extends CategoryList {
-  constructor(name: string, categories: MainCategory[]) {
-    super(name, categories);
-  }
-}
--- src/app-logic/ProjectList.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-useless-constructor */
-import CategoryList from "./CategoryList";
-import Project from "./MainCategory";
-
-export default class ProjectList extends CategoryList {
-  constructor(name: string, projectItems: Project[]) {
-    super(name, projectItems);
-  }
-}
