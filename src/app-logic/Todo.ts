b8ca8e3f08abab4699e2660696c1aad0c21ac767
Charbytesdev <138222011+Charbytesdev@users.noreply.github.com>
2023-10-12 13:51:29 +0300

    Store todo from user input

--- src/app-logic/Todo.ts
+++ src/app-logic/Todo.ts
@@ -1,21 +1,41 @@
+import { Priority } from "../app-util/utility";
 import NamedIdentifiable from "./NamedIdentifiable";
-
-type Priority = "low" | "medium" | "high";
 
 export default class Todo extends NamedIdentifiable {
   private _checked: boolean;
 
   private _description: string;
 
-  private _dueDate: string;
+  private _dueDate: Date;
 
   private _priority: Priority;
+
+  public static from(
+    inputCollection: HTMLCollectionOf<HTMLInputElement>
+  ): Todo | void {
+    const arr = Array.from(inputCollection);
+    let inputs: string[] = [];
+    let priority: Priority = "low";
+    for (const item of arr.slice(0, 3)) {
+      if (item === arr[2]) {
+      }
+      inputs.push(item.value);
+    }
+    for (const item of arr.slice(3)) {
+      if (item.checked) {
+        priority =
+          item.nextElementSibling?.textContent?.toLowerCase() as Priority;
+      }
+    }
+
+    return new Todo(false, inputs[0], inputs[1], new Date(inputs[2]), priority);
+  }
 
   constructor(
     checked: boolean,
     name: string,
     description: string,
-    dueDate: string,
+    dueDate: Date,
     priority: Priority
   ) {
     super(name);
@@ -41,11 +61,11 @@ export default class Todo extends NamedIdentifiable {
     this._description = description;
   }
 
-  public get dueDate(): string {
+  public get dueDate(): Date {
     return this._dueDate;
   }
 
-  public set dueDate(dueDate: string) {
+  public set dueDate(dueDate: Date) {
     this._dueDate = dueDate;
   }
 
